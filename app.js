50d05d34a1e8292116afa184a63fd16258c84d7e
Olwyn <odonogol@tcd.ie>
2023-12-29 23:16:33 +0000

    expiring note updates

--- app.js
+++ app.js
@@ -4,6 +4,12 @@ function saveNotesToStorage() {
     localStorage.setItem('notes', JSON.stringify(notes));
 }
 
+function isNoteExpired(noteTimestamp) {
+    const threeDaysInMilliseconds = 3 * 24 * 60 * 60 * 1000;
+    const age = Date.now() - new Date(noteTimestamp).getTime();
+    return age > threeDaysInMilliseconds;
+}
+
 function createNote() {
     const content = document.getElementById('new-note-content').value;
     if (!content) {
@@ -11,7 +17,7 @@ function createNote() {
         return;
     }
 
-    const colors = ['pink', 'light blue', 'yellow', 'orange', 'green'];
+    const colors = ['#EBDEF0', '#D6EAF8', '#F5B7B1', '#F9E79F', '#D1F2EB', '#DAF7A6'];//fix colours
     const color = colors[Math.floor(Math.random() * colors.length)];
     const note = { id: Date.now(), content, color, timestamp: new Date() };
 
@@ -25,6 +31,14 @@ function displayNotes() {
     const container = document.getElementById('notes-container');
     container.innerHTML = '';
     notes.forEach(note => {
+        if (isNoteExpired(note.timestamp)) {
+            // Remove expired note and continue to next iteration
+            const noteIndex = notes.indexOf(note);
+            notes.splice(noteIndex, 1);
+            saveNotesToStorage();
+            return; // Skip the rest of this iteration
+        }
+
         const noteElement = document.createElement('div');
         noteElement.className = 'note';
         noteElement.style.backgroundColor = note.color;
@@ -42,7 +56,12 @@ function displayNotes() {
         deleteButton.onclick = function() { deleteNote(note.id); };
         noteElement.appendChild(deleteButton);
 
-        // Append the note element to the container
+        // Create and append the remaining time
+        const timeRemaining = document.createElement('div');
+        timeRemaining.className = 'time-remaining';
+        timeRemaining.textContent = `Expires in: ${calculateRemainingTime(note.timestamp)}`;
+        noteElement.appendChild(timeRemaining);
+
         container.appendChild(noteElement);
     });
 }
@@ -54,6 +73,19 @@ function deleteNote(noteId) {
         saveNotesToStorage();
         displayNotes();
     }
+}
+
+function calculateRemainingTime(noteTimestamp) {
+    const expirationTime = new Date(noteTimestamp).getTime() + (3 * 24 * 60 * 60 * 1000); // 3 days in milliseconds
+    const currentTime = Date.now();
+    const timeLeft = expirationTime - currentTime;
+
+    // Convert time left from milliseconds to a more readable format
+    const days = Math.floor(timeLeft / (24 * 60 * 60 * 1000));
+    const hours = Math.floor((timeLeft % (24 * 60 * 60 * 1000)) / (60 * 60 * 1000));
+    const minutes = Math.floor((timeLeft % (60 * 60 * 1000)) / (60 * 1000));
+
+    return days > 0 ? `${days}d ${hours}h` : `${hours}h ${minutes}m`;
 }
 
 // Call displayNotes on page load
